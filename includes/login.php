d29676655e67b1b9205e80ff7199e5220cdf5a59
joaor1os <joaoarios@hotmail.com>
2024-08-05 10:21:01 -0300

    alteracao login

--- includes/login.php
+++ includes/login.php
@@ -6,17 +6,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha = $_POST['senha'];
     $tipo_usuario = $_POST['tipo_usuario'];
 
-    $sql = "SELECT id_usuario, tipo_usuario FROM usuarios WHERE email = ? AND senha = ? AND tipo_usuario = ?";
+    // Verificar a tabela correspondente com base no tipo de usuário
+    switch ($tipo_usuario) {
+        case 1: // Instituição
+            $sql = "SELECT id_inst, email, senha FROM instituicao WHERE email = ? AND senha = ?";
+            break;
+        case 2: // Professor
+            $sql = "SELECT id_professor, email, senha FROM professor WHERE email = ? AND senha = ?";
+            break;
+        case 3: // Aluno
+            $sql = "SELECT matricula_aluno, email, senha FROM aluno WHERE email = ? AND senha = ?";
+            break;
+        default:
+            header("location: ../login.html?error=1");
+            exit();
+    }
+
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssi", $email, $senha, $tipo_usuario);
+    $stmt->bind_param("ss", $email, $senha);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id_usuario, $db_email, $db_senha);
+        $stmt->fetch();
+
         session_start();
         $_SESSION['loggedin'] = true;
         $_SESSION['email'] = $email;
         $_SESSION['tipo_usuario'] = $tipo_usuario;
+        $_SESSION['id_usuario'] = $id_usuario; // Adicionando o ID do usuário à sessão
         header("location: ../index.php");
     } else {
         header("location: ../login.html?error=1");
